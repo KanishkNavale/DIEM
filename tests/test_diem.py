657c66c53cf34f16b8106d05428ef64dae6d1c50
kanishk <navalekanishk@gmail.com>
2024-09-01 15:35:58 +0200

    Add: base test of vector dimension 1M

---
+++ tests/test_diem.py
@@ -0,0 +1,18 @@
+import unittest
+
+import torch
+
+from diem import DIEM
+
+
+class TestDiem(unittest.TestCase):
+    def test_high_dim_vectors(self):
+        pick_index = 3
+
+        a = torch.randn(10, 1000_000)
+        b = a[pick_index].unsqueeze(dim=0)
+
+        result = DIEM(a, b)
+        most_similar = torch.argmin(result, dim=0)
+
+        self.assertEqual(most_similar, pick_index)

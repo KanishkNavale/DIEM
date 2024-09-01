a8f0dbb2189936c847d81e7acb6c8213c29f7880
kanishk <navalekanishk@gmail.com>
2024-09-01 15:24:25 +0200

    Add: the metric funtion

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ This repository implement the distance measure metric DIEM.
 
 source: [https://arxiv.org/abs/2407.08623](https://arxiv.org/abs/2407.08623)
 
+Note: Please cite the original paper.
+
 ## Development
 
 ```bash
---
+++ diem/__init__.py
@@ -0,0 +1,35 @@
+import math
+
+import torch
+
+
+def DIEM(source: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    """
+    Source: https://arxiv.org/pdf/2407.08623
+
+    Computes the DIEM distance between two tensors.
+    DIEM: Dimension Insenstiive Embedding Metric
+
+    Args:
+        source (torch.Tensor): The source tensor of shape (N, D).
+        target (torch.Tensor): The target tensor of shape (M, D).
+
+    Returns:
+        torch.Tensor: The DIEM distance between the source and target tensors of shape (N, M).
+
+    """
+    if source.size(1) != target.size(1):
+        raise ValueError(
+            "The source and target tensors must have the same dimension."
+        )
+    D = source.size(1)
+
+    v_min = torch.min(torch.cat([source, target]))
+    v_max = torch.max(torch.cat([source, target]))
+
+    euclidean_dist = torch.cdist(source, target, p=2)
+
+    expected_value = math.sqrt(D / 6) * (v_max - v_min)
+    variance = (D / 6) * ((v_max - v_min) ** 2) / 3.0
+
+    return (v_max - v_min) / variance * (euclidean_dist - expected_value)
